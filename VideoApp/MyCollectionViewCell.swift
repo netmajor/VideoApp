83f05a61c0f095ebbea6b39f14b820894c1d20aa
netmajor <Netmajor@MacBook-Pro-Lukasz.local>
2017-09-25 21:25:02 +0200

    Init commit with simple collection view

---
+++ VideoApp/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  VideoApp
+//
+//  Created by Lukasz Bartczak on 23.09.2017.
+//  Copyright © 2017 Lukasz Bartczak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+}
