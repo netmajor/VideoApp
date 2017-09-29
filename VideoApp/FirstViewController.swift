b4bb153fc434282e54dc98246b6a8bde433671ff
netmajor <Netmajor@MacBook-Pro-Lukasz.local>
2017-09-29 22:35:37 +0200

    Vertical cells

--- VideoApp/FirstViewController.swift
+++ VideoApp/FirstViewController.swift
@@ -8,39 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate {
+class FirstViewController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
     let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
     var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
     
     
     // MARK: - UICollectionViewDataSource protocol
     
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return items.count/10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height:200)
+    }
+    
     // tell the collection view how many cells to make
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items.count
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
     }
     
     // make a cell for each cell index path
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // get a reference to our storyboard cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath)
+        cell.backgroundColor = UIColor.red
         
         // Use the outlet in our custom class to get a reference to the UILabel in the cell
-        cell.myLabel.text = self.items[indexPath.item]
-        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
         
         return cell
     }
     
     // MARK: - UICollectionViewDelegate protocol
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // handle tap events
         print("You selected cell #\(indexPath.item)!")
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "VIDEOS"
+        collectionView?.backgroundColor = UIColor.blue
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
