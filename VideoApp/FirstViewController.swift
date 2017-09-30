64e532f004d5a796de52a3e6510bc9c1e731bae7
netmajor <Netmajor@MacBook-Pro-Lukasz.local>
2017-09-30 22:07:50 +0200

    Get photos from iPhone and showing them

--- VideoApp/FirstViewController.swift
+++ VideoApp/FirstViewController.swift
@@ -7,16 +7,17 @@
 //
 
 import UIKit
+import Photos
 
 class FirstViewController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
     let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
-    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
     
-    
+    var uiImageArray : [UIImage] = []
+
     // MARK: - UICollectionViewDataSource protocol
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return items.count/10
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -25,7 +26,7 @@ class FirstViewController: UICollectionViewController,UICollectionViewDelegateFl
     
     // tell the collection view how many cells to make
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 7
+        return uiImageArray.count
     }
     
     // make a cell for each cell index path
@@ -36,7 +37,12 @@ class FirstViewController: UICollectionViewController,UICollectionViewDelegateFl
         cell.backgroundColor = UIColor.red
         
         // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        let imageView = UIImageView(frame: CGRect(x:0, y:0, width:cell.frame.size.width, height:cell.frame.size.height))
+        let image = uiImageArray[indexPath.row]
+        imageView.image = image
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
         
+        cell.addSubview(imageView)
         return cell
     }
     
@@ -46,13 +52,40 @@ class FirstViewController: UICollectionViewController,UICollectionViewDelegateFl
         // handle tap events
         print("You selected cell #\(indexPath.item)!")
     }
+    
+    fileprivate func getAssets() {
+        let options = PHFetchOptions()
+        options.sortDescriptors = [ NSSortDescriptor(key: "creationDate", ascending: true) ]
+        options.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
+        let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: options)
+        assets.enumerateObjects { (obj, idx, bool) -> Void in
+            self.uiImageArray.append(self.getAssetThumbnail(asset: obj))
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "VIDEOS"
         collectionView?.backgroundColor = UIColor.blue
         collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView?.dataSource = self
+        getAssets()
+        /*
+        var cameraRollAssets = results.filteredArrayUsingPredicate(NSPredicate(format: "assetSource == %@", argumentArray: [3]))
+        results = NSMutableArray(array: cameraRollAssets)*/
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+   fileprivate func getAssetThumbnail(asset: PHAsset) -> UIImage {
+        let manager = PHImageManager.default()
+        let option = PHImageRequestOptions()
+        var thumbnail = UIImage()
+        option.isSynchronous = true
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
+            thumbnail = result!
+        })
+        return thumbnail
     }
 
     override func didReceiveMemoryWarning() {
